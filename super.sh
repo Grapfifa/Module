5b25cd872fcb49d97994fcc698d798523b82114a
Grapfifa <levietdaithang123@gmail.com>
2025-07-04 09:27:24 +0700

    Update super.sh

--- super.sh
+++ super.sh
@@ -1,1 +1,12 @@
-
+[
+  {
+    "name": "demo",
+    "version": "V1",
+    "raw": "https://raw.githubusercontent.com/binhpt567/ttp-ai/refs/heads/main/demo.sh"
+  },
+  {
+   "name": "SensiXPatch",
+    "version": "V2",
+    "raw": ""
+  }
+]
