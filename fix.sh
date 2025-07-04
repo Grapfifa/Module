2df56f00ded7c800bbc78b4f9fd42326d5d9d7a1
Grapfifa <levietdaithang123@gmail.com>
2025-07-04 09:29:50 +0700

    fix.sh

---
+++ fix.sh
@@ -0,0 +1,16 @@
+echo "
+                 █▀ █░█ █▀█ █▀▀ █▀█
+                 ▄█ █▄█ █▀▀ ██▄ █▀▄
+
+         █▀▀ ▄▀█ █▀▄▀█ █ █▄░█ █▀▀ █░█ ▄█
+         █▄█ █▀█ █░▀░█ █ █░▀█ █▄█ ▀▄▀ ░█"
+echo ".      🔰 DEV: GRAP FIFA / YTB: GRAP FIFA📂"
+echo ".                👽Zalo: Thắng Lê👽"
+    echo ".  📱 Thông tin thiết bị 🔍"
+    echo "-----------------------"
+    sleep 0.3
+    echo ".  👾 - Phần cứng:              $(getprop ro.hardware)"
+    sleep 0.3
+    echo ".  👾 Phiên bản Android  ➜ $(getprop ro.build.version.release)"
+    sleep 0.3
+    echo ".  👾 Device Model      ➜ $(getprop ro.product.model)"
