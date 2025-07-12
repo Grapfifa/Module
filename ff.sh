59294f25d8aa722b1c0f2b01d04544bc2df77a6e
Hackacczggzvsshbsbshj <levietdaithang123@gmail.com>
2025-07-12 21:59:14 +0700

    ff.sh

--- ff.sh
+++ ff.sh
@@ -5,52 +5,48 @@ echo "🚀Module fix lag freefireth và freefiremax🚀"
 echo "📂Chức năng chính:"
 echo "→Xóa skin súng✔"
 sleep 1
-echo "→Xóa văn bản✔"
-sleep 1
 echo "→Xóa hình ảnh✔"
 sleep 1
 echo "→Xóa tự động cập nhật gói mở rộng✔"
-fixlag() {
-    for pkg in "com.dts.freefireth" "com.dts.freefiremax"; do
-        base_path="/storage/emulated/0/Android/data/${pkg}/files/contentcache/Optional/android"
-        folders=(
-            "gameassetbundles"
-            "optionalavatarres/gameassetbundles"
-            "optionalfootballres/gameassetbundles"
-            "optionalinfection/gameassetbundles"
-            "optionalingameres/gameassetbundles"
-            "optionallobbyres/gameassetbundles"
-            "optionallocres/gameassetbundles"
-            "optionallonewolfres/gameassetbundles"
-            "optionalmap1res/gameassetbundles"
-            "optionalmap4res/gameassetbundles"
-            "optionalmaphippores/gameassetbundles"
-            "optionalmapponyres/gameassetbundles"
-            "optionalmapres/gameassetbundles"
-            "optionalnewblast/gameassetbundles"
-            "optionalpetres/gameassetbundles"
-            "optionalrushingpetsres/gameassetbundles"
-            "optionalsocialres/gameassetbundles"
-            "optionaltrainingres/gameassetbundles"
-            "optionalugcres/gameassetbundles"
-            "optionalvoiceres/gameassetbundles"
-            "optionalwerunres/gameassetbundles"
-        )
-
-        for folder in "${folders[@]}"; do
-            full_path="${base_path}/${folder}"
-            rm -rf "$full_path"
-            touch "$full_path"
-            echo "Thang_Le_uy_tin" > "$full_path"          
-        done
-
-        cdnicon_path="/storage/emulated/0/Android/data/${pkg}/files/ImageCache/CDNICON"
-        rm -rf "$cdnicon_path"
-        touch "$cdnicon_path"
-        echo "Thang_Le_uy_tín" > "$cdnicon_path"
-    done
-}
-fixlag > /dev/null 2>&1     
+lag_fix() {
+    rm -rf /sdcard/Android/data/com.dts.freefireth/cache
+    rm -rf /sdcard/Android/data/com.dts.freefiremax/cache
+    rm -rf /sdcard/Android/data/com.dts.freefireth/files/ffrtc_log.txt
+    rm -rf /sdcard/Android/data/com.dts.freefireth/files/ffrtc_log_bak.txt
+    rm -f /sdcard/Android/data/com.dts.freefiremax/files/ffrtc_log.txt
+    rm -f /sdcard/Android/data/com.dts.freefiremax/files/ffrtc_log_bak.txt
+    touch /sdcard/Android/data/com.dts.freefireth/cache
+    touch /sdcard/Android/data/com.dts.freefiremax/cache
+    echo "grapfifa" > sdcard/Android/data/com.dts.freefireth/cache
+    echo "grapfifa" > sdcard/Android/data/com.dts.freefiremax/cache
+    rm -rf /sdcard/Android/data/com.dts.freefireth/files/ImageCache/2.109
+    touch /sdcard/Android/data/com.dts.freefireth/files/ImageCache/2.109
+    echo "grapfifa" > sdcard/Android/data/com.dts.freefireth/files/ImageCache/2.109
+    rm -rf /sdcard/Android/data/com.dts.freefiremax/files/ImageCache/2.109
+    touch /sdcard/Android/data/com.dts.freefiremax/files/ImageCache/2.109
+    echo "grapfifa" > sdcard/Android/data/com.dts.freefiremax/files/ImageCache/2.109
+    rm -rf /sdcard/Android/data/com.dts.freefireth/files/ImageCache/CDNICON
+    touch /sdcard/Android/data/com.dts.freefireth/files/ImageCache/CDNICON
+    echo "grapfifa" > sdcard/Android/data/com.dts.freefireth/files/ImageCache/CDNICON
+    rm -rf /sdcard/Android/data/com.dts.freefiremax/files/ImageCache/CDNICON
+    touch /sdcard/Android/data/com.dts.freefiremax/files/ImageCache/CDNICON
+    echo "grapfifa" > sdcard/Android/data/com.dts.freefiremax/files/ImageCache/CDNICON
+    echo "grapfifa" > storage/emulated/0/Android/data/com.dts.freefiremax/files/contentcache/Temp/android/MultiThread/OptionalClothRes/gameassetbundles/optionalab_weapon_014.63~2BaZgT2gA1nC6O2xR9ziBrZp5M~3D.flag
+    echo "grapfifa" > storage/emulated/0/Android/data/com.dts.freefiremax/files/contentcache/Optional/android/fileinfo
+    echo "grapfifa" > storage/emulated/0/Android/data/com.dts.freefiremax/files/contentcache/Optional/android/optionalvoiceres/fileinfo
+    echo "grapfifa" > storage/emulated/0/Android/data/com.dts.freefiremax/files/contentcache/Optional/android/optionalugcres/fileinfo
+    echo "grapfifa" > storage/emulated/0/Android/data/com.dts.freefiremax/files/contentcache/Optional/android/optionallocres/gameassetbundles/map_vn.xBxp01d2P7BMLyJzGrAZDL7PezM~3D
+    echo "grapfifa" > storage/emulated/0/Android/data/com.dts.freefiremax/files/contentcache/Optional/android/optionallocres/gameassetbundles/open_sound_vn.bQNfHGUtHqcT27GNL2eZxtIcA4s~3D
+    echo "grapfifa" > storage/emulated/0/Android/data/com.dts.freefiremax/files/contentcache/Optional/android/optionallocres/gameassetbundles/vo_ingame_bombsquad_vn.BXLEC3umA8tvjNvxc8XUeJcwZk4~3D
+    echo "grapfifa" > storage/emulated/0/Android/data/com.dts.freefireth/files/contentcache/Temp/android/MultiThread/OptionalClothRes/gameassetbundles/optionalab_weapon_014.63~2BaZgT2gA1nC6O2xR9ziBrZp5M~3D.flag
+    echo "grapfifa" > storage/emulated/0/Android/data/com.dts.freefireth/files/contentcache/Optional/android/fileinfo
+    echo "grapfifa" > storage/emulated/0/Android/data/com.dts.freefireth/files/contentcache/Optional/android/optionalvoiceres/fileinfo
+    echo "grapfifa" > storage/emulated/0/Android/data/com.dts.freefireth/files/contentcache/Optional/android/optionalugcres/fileinfo
+    echo "grapfifa" > storage/emulated/0/Android/data/com.dts.freefireth/files/contentcache/Optional/android/optionallocres/gameassetbundles/map_vn.xBxp01d2P7BMLyJzGrAZDL7PezM~3D
+    echo "grapfifa" > storage/emulated/0/Android/data/com.dts.freefireth/files/contentcache/Optional/android/optionallocres/gameassetbundles/open_sound_vn.bQNfHGUtHqcT27GNL2eZxtIcA4s~3D
+    echo "grapfifa" > storage/emulated/0/Android/data/com.dts.freefireth/files/contentcache/Optional/android/optionallocres/gameassetbundles/vo_ingame_bombsquad_vn.BXLEC3umA8tvjNvxc8XUeJcwZk4~3D 
+} 
+lag_fix > /dev/null 2>&1
 echo "Đã tối ưu thành công bạn có thể vào 🎮"
 sleep 1
 echo "An toàn 100% ✔👌👍"
